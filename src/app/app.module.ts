1165004833a80098fdf1d0c254923af612eb05d0
597936721@qq.com <19921005d>
2017-09-11 11:18:03 +0800

    router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,31 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 import { HeroDetailComponent } from './hero-detail.component';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
+    HeroesComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent,
+      },
+    ])
   ],
-  providers: [],
+  providers: [
+    HeroService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
